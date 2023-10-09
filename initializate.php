9818e3c2866c42f44f2f09fe3ad0f147d96d2f14
krupann <krup04031983@gmail.com>
2023-10-10 00:32:22 +0400

    Update initializate.php

--- initializate.php
+++ initializate.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_SESSION['users'])) {
     $_SESSION['users'] = array();
-    array_push($_SESSION['users'], new User(0, 'user','','qwerty227', new Adress('','')));
+    array_push($_SESSION['users'], new User(0, 'user','','qwerty227', new Adress('Russia','')));
     $_SESSION['id'] = 0;
 }
 ?>
